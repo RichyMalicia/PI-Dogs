28d9b0f0a4baad5f844cd6f38ee566e71d730036
ricardoJorge <rdjorge@live.com.ar>
2022-07-03 19:59:33 -0300

    modificado

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home/Home';
 import LandingPage from './components/LandingPage/LandingPage';
 import { Route } from 'react-router-dom';
 import DogDetail from './components/DogDetail/DogDetail';
+import DogCreate from './components/DogCreate/DogCreate';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
       
       <Route exact path="/" component={LandingPage}/>
       <Route exact path="/home" component={Home}/>
+      <Route exact path='/createDog' component={DogCreate}/>
       <Route path='/detail/:id' component={DogDetail}/>
       
     </div>
