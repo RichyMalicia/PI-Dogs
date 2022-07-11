8aac524153a9b023310196c6ee7ef708e3ac4613
ricardoJorge <rdjorge@live.com.ar>
2022-07-11 11:05:28 -0300

    make it better

--- api/src/db.js
+++ api/src/db.js
@@ -33,6 +33,14 @@ let sequelize =
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
